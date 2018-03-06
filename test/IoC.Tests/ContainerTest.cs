30bb630752fa807cd8b43dca9b086219613afd35
Baylor Rae <baylor@thecodedeli.com>
2018-03-05 20:43:15 -0600

    add sln and projects

---
+++ test/IoC.Tests/ContainerTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace IoC.Tests
+{
+    public class ContainerTestBase
+    {
+    }
+
+    [TestFixture]
+    public class Container_GetInstance : ContainerTestBase
+    {
+        [Test]
+        public void CreatesAnInstanceWithParams()
+        {
+        }
+    }
+}
