cad5803d6ca6824f75ad1ed5da0550036dfb61d4
Baylor Rae <baylor@thecodedeli.com>
2018-03-05 21:58:21 -0600

    can initialize a new object

---
+++ src/IoC/Container.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IoC
+{
+    public class Container
+    {
+        public object GetInstance(Type type)
+        {
+            return Activator.CreateInstance(type);
+        }
+    }
+}
--- test/IoC.Tests/ContainerTest.cs
+++ test/IoC.Tests/ContainerTest.cs
@@ -1,17 +1,36 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
 
 namespace IoC.Tests
 {
     public class ContainerTestBase
     {
+        protected Container Container;
+
+        [SetUp]
+        public void BeforeEach()
+        {
+            Container = new Container();
+        }
+
+        [TearDown]
+        public void AfterEach()
+        {
+            Container = null;
+        }
     }
 
     [TestFixture]
     public class Container_GetInstance : ContainerTestBase
     {
         [Test]
-        public void CreatesAnInstanceWithParams()
+        public void CreatesAnInstanceWithNoParams()
         {
+            var subject = (A) Container.GetInstance(typeof(A));
+            subject.Should().BeOfType<A>();
         }
+
+        class A
+        { }
     }
 }
